b051cd0fdfd3587c9eea1860c422b21616ea5fd1
Mike Wang <163wjma@163.com>
2016-03-01 14:41:24 +0800

    修复错误

--- test7.cpp
+++ test7.cpp
@@ -35,56 +35,8 @@ T CQueue<T>:: deleteHead()
         }
     }
     T t = stack2.top();
-    T temp = stack2.top();
     stack2.pop();
-    return temp;
-
-}
-int main()
-{
-
-    return 0;
-}
-#include <iostream>
-#include <stack>
-
-using namespace std;
-
-template <typename T>
-class CQueue
-{
-public:
-    CQueue(void);
-    ~CQueue(void);
-
-    void appendTail(const T& node);
-    T deleteHead(void);
-private:
-    stack<T> stack1;
-    stack<T> stack2;
-};
-
-template <typename T>
-void CQueue<T>::appendTail(const T& node)
-{
-    stack1.push(node);
-}
-
-template <typename T>
-T CQueue<T>:: deleteHead()
-{
-    if (stack2.empty())
-    {
-        while (!stack1.empty())
-        {
-            stack2.push(stack1.top());
-            stack1.pop();
-        }
-    }
-    T t = stack2.top();
-    T temp = stack2.top();
-    stack2.pop();
-    return temp;
+    return t;
 
 }
 int main()
