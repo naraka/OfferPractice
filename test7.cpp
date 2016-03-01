93ff70e4c04e93ddcd847d75ba1e2a72999971e6
Mike Wang <163wjma@163.com>
2016-03-01 14:38:28 +0800

    修复编译错误
    模版类语法错误

--- test7.cpp
+++ test7.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-template <typename T> 
+template <typename T>
 class CQueue
 {
 public:
     CQueue(void);
     ~CQueue(void);
-    
+
     void appendTail(const T& node);
     T deleteHead(void);
 private:
@@ -18,30 +18,77 @@ private:
 };
 
 template <typename T>
-void CQueue::appendTail(const T& node)
+void CQueue<T>::appendTail(const T& node)
 {
     stack1.push(node);
 }
 
 template <typename T>
-T CQueue:: deleteHead()
+T CQueue<T>:: deleteHead()
 {
     if (stack2.empty())
     {
         while (!stack1.empty())
         {
             stack2.push(stack1.top());
-            stack.pop();
+            stack1.pop();
         }
     }
     T t = stack2.top();
     T temp = stack2.top();
     stack2.pop();
-    return temp;    
-    
+    return temp;
+
 }
 int main()
 {
-    
+
+    return 0;
+}
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+template <typename T>
+class CQueue
+{
+public:
+    CQueue(void);
+    ~CQueue(void);
+
+    void appendTail(const T& node);
+    T deleteHead(void);
+private:
+    stack<T> stack1;
+    stack<T> stack2;
+};
+
+template <typename T>
+void CQueue<T>::appendTail(const T& node)
+{
+    stack1.push(node);
+}
+
+template <typename T>
+T CQueue<T>:: deleteHead()
+{
+    if (stack2.empty())
+    {
+        while (!stack1.empty())
+        {
+            stack2.push(stack1.top());
+            stack1.pop();
+        }
+    }
+    T t = stack2.top();
+    T temp = stack2.top();
+    stack2.pop();
+    return temp;
+
+}
+int main()
+{
+
     return 0;
 }
